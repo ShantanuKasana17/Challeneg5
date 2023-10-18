e1737ec13b82a2772e39fae78196232d77810e08
Shantanu <shantanukasana017@gmail.com>
2023-10-18 16:30:56 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React from 'react';
+import{add,div,mult,sub} from "./Calc";
+function App(){
+  return(
+  <>
+  <ul>
+      <li> sum of two no is {add(40,4)}</li>
+      <li> sub of two no is {sub(40,4)}</li>
+      <li> div of two no is {div(40,4)}</li>
+      <li> mult of two no is {mult(40,4)}</li>
+      
+    </ul>
+  </>
   );
+    
 }
-
-export default App;
+export default App
